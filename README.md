2cb9877a40ff086d32a9e1f4b61e2a9c43212abb
Tim Lightfoot <tim.lightfoot@gmail.com>
2020-06-01 11:11:45 +0800

    refactor code and update README

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 # md
 
+## About
+
 Zero configuration minimal markdown server for local rendering.
+
+## Features
+
+* fast start with zero or minimal CLI config
+* aeroplane mode (no external assets)
 
 ## Installation
 
+via go get:
 ```
 $ go get github.com/tlight/md
+```
+
+via gobinaries (if you don't have Go installed):
+```
+curl -sf https://gobinaries.com/tlight/md | sh
 ```
 
 ## Usage
 
 ```sh
-> md README.md
+$ md README.md
 Starting Markdown Server for 'README.md' at http://localhost:8080
+
+$ md README.md --port 3000
+Starting Markdown Server for 'README.md' at http://localhost:3000
 ```
 
 # License
---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/yuin/goldmark"
+)
+
+// MarkdownHandler contains code & configuration
+type MarkdownHandler struct {
+	Filename string
+	Markdown string
+	HTML     template.HTML // as converted from Markdown
+	Template *template.Template
+}
+
+// NewMarkdownHandler returns obj
+func NewMarkdownHandler(filename string) *MarkdownHandler {
+	t, err := template.New("md").Parse(string(Client))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &MarkdownHandler{
+		Filename: filename,
+		Template: t,
+	}
+}
+
+// ServeHTTP implements the Handler interface to respond to request by converting markdown & rendering html
+func (s *MarkdownHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("GET /")
+	s.Refresh()
+	s.Template.Execute(w, s)
+}
+
+// Refresh updates the file Input Markdown & Output HTML stored in the struct
+func (s *MarkdownHandler) Refresh() {
+	markdown, err := ioutil.ReadFile(s.Filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Markdown = string(markdown)
+	var html bytes.Buffer
+	if err := goldmark.Convert([]byte(s.Markdown), &html); err != nil {
+		log.Println("ERROR: Unable to parse Markdown")
+	}
+	s.HTML = template.HTML(html.String())
+}
